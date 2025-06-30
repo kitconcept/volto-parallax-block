2fd97738e662fecc38a9b4a2732b1a13d47a8236
Jonas Piterek <piterek@kitconcept.com>
2025-06-30 10:35:19 +0200

    fix .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,24 +20,42 @@ module.exports = {
   extends: `${coreLocation}/packages/volto/.eslintrc`,
   rules: {
     'import/no-unresolved': 1,
+    'import/named': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+    'no-restricted-imports': [
+      'error',
+      {
+        name: '@plone/volto/components',
+        message:
+          'Importing from barrel files is not allowed. Please use direct imports of the modules instead.',
+      },
+      {
+        name: '@plone/volto/helpers',
+        message:
+          'Importing from barrel files is not allowed. Please use direct imports of the modules instead.',
+      },
+      {
+        name: '@plone/volto/actions',
+        message:
+          'Importing from barrel files is not allowed. Please use direct imports of the modules instead.',
+      },
+    ],
+    'react/jsx-key': [2, { checkFragmentShorthand: true }],
   },
   settings: {
     'import/resolver': {
-      node: {
-        paths: ['src'],
-        alias: {
-          map: [
-            ['@plone/volto', `${coreLocation}/packages/volto/src`],
-            ['@plone/volto-slate', `${coreLocation}/packages/volto-slate/src`],
-            ['@plone/registry', `${coreLocation}/packages/registry/src`],
-            [
-              '@kitconcept/volto-parallax-block',
-              './packages/volto-parallax-block/src',
-            ],
-            ...addonAliases,
+      alias: {
+        map: [
+          ['@plone/volto', `${coreLocation}/packages/volto/src`],
+          ['@plone/volto-slate', `${coreLocation}/packages/volto-slate/src`],
+          ['@plone/registry', `${coreLocation}/packages/registry/src`],
+          [
+            '@kitconcept/volto-parallax-block',
+            './packages/volto-parallax-block/src',
           ],
-          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-        },
+          ...addonAliases,
+        ],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       },
     },
   },
